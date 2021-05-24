04ef97f70573d58b47200867b0284287673d62e9
Hisenzerg <mrhisenberg100@gmail.com>
2021-05-23 23:16:25 -0400

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,13 @@ $(function () {
     fade: true,
   });
 
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+
+  });
+
 
 });
